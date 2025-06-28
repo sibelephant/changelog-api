9589b7de3ce91c70f2e0bba0e91cdfafa3980f76
sibelephant <masterpiekhalid@gmail.com>
2025-06-28 08:02:22 +0100

    Add validation for product update endpoint

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { body, validationResult } from "express-validator";
 
 const router = Router()
 
@@ -14,7 +15,14 @@ router.get("/product/:id", (req, res) => {});
 
 router.post("/product", (req, res) => {});
 
-router.put("/product/:id", (req, res) => {});
+router.put("/product/:id",body('name'), (req, res) => {
+  const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+    res.status(400);
+    res.json({ errors: errors.array() });
+  }
+});
 
 router.delete("/product/:id", (req, res) => {});
 
